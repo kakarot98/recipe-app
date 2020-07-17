fb018eed5fa84393f73be3af0a277235ccb1a131
kakarot98 <hrushikeshmohanty93@gmail.com>
2020-07-17 21:34:59 +0530

    added api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+//import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "23a086bd";
   const APP_KEY = "5031e1c29d48a95e28341fcad47fa3c5";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form>
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
